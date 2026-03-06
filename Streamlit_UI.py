b3e261141f40edc849de1ee2bf1216ba76ede52b
unknown <Elouan180@free.fr>
2026-03-06 10:21:26 -0500

    Pemier pas vers l'app

--- Streamlit_UI.py
+++ Streamlit_UI.py
@@ -1,1 +1,11 @@
 import streamlit as st
+import pandas as pa
+
+path = r"C:\Users\ELOUAN\Desktop\INRS\Culture Follow up\Culture_data.xlsx"
+
+def load_data(path):
+    data = pa.read_excel(path)
+    return data
+
+st.text("Hello, welcome to the Culture Follow-up Dashboard !")
+st.text(load_data(path))
